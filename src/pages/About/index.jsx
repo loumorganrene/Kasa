79473fe6bab04705c2d05992da7906172cf7d991
Lou René <75613368+loumorganrene@users.noreply.github.com>
2023-01-13 09:13:15 +0100

    Banner components Home+About Done

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,9 +1,14 @@
 import Header from '../../components/Header'
+import Banner from '../../components/Banner'
+import HeroImg from '../../assets/about_hero_img_d.png'
 function About() {
     return (
         <div>
             <Header />
-            <h1> Page A propos</h1>
+            <Banner>
+                <img className="home_hero_img" src={HeroImg} alt="Home Hero" />
+                <div className="transparent" />
+            </Banner>
         </div>
     )
 }
